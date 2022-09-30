6c317a5a649fb17eaae866e7de63f32b18370c4e
Omair Nabiel <nabiel.omair@gmail.com>
2022-09-30 07:18:52 +0500

    chore: init a svelte project for frontend

--- contract/contract.sol
+++ contract/contract.sol
@@ -1,11 +1,27 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.17;
 
 contract Campaign {
+
+	struct Request {
+		uint approvalCount;
+		string description;
+		uint value;
+		bool complete;
+		address recipient;
+		mapping(address => bool) approvals;
+	}
+
 	address public manager;
 	uint public minimumContribution;
+	mapping(address => bool) public approvers;
 
-	function Campaign(uint minimum) public {
+	constructor(uint minimum) {
 		manager = msg.sender;
 		minimumContribution = minimum;
+	}
+
+	function contribute() public payable{
+		require(msg.value > minimumContribution);
+		approvers[msg.sender] = true;
 	}
 }
